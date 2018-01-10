170eff42ef569d5d63ad180666f1f03b02bd097c
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-10 20:05:05 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,8 @@
  }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each {
-    puts 
+  birthday_kids.each { |kids_name,age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   }
 end
 
